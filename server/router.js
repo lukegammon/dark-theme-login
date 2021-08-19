7ed249f8306140a544523a9653512e76619807cf
Luke Gammon <lukegammon@gmx.com>
2021-08-19 15:53:46 +0100

    added handlebars templating

--- server/router.js
+++ server/router.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 
 //bcrypt settings
 const bcrypt = require("bcrypt");
-const e = require("express");
 const saltRounds = 10;
 
 // add user to db
@@ -39,14 +38,13 @@ const userExists = async (email) => {
 }
 
 router.get("/", (req, res) => {
-    res.sendFile("login.html", {root: 
-        "../client/views"});
+    res.render("login.handlebars");
 });
 
 router.post("/auth/login", async (req, res) => {
     const formValidated = await loginValidation(req.body.email, req.body.password);
     if(formValidated === true) {
-        res.send("Login Validation Successfull");
+        res.render("dashboard", { name: req.body.email });
     } else {
         console.error(formValidated);
     }
@@ -58,12 +56,13 @@ router.post("/auth/signup", async (req, res) => {
         const email = req.body.email;
         const exists = await userExists(email);
         if(exists) {
+            // reload page and display user already exists error
             console.log("Redirect to user exists page");
-            // stop form submitting and display user already exists page
         } else {
             addUser(email, req.body.password);
             // email verification
             // redirect to verify your email and login page
+            // pass in please login message to login page
             res.sendFile("login.html", {root: "../client/views"});
         }
         
@@ -73,8 +72,7 @@ router.post("/auth/signup", async (req, res) => {
 })
 
 router.get("/signup", (req,res) => {
-    res.sendFile("signup.html", {root: 
-        "../client/views"});
+    res.render("signup.handlebars");
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,17 +3,20 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config()
 const router = require("./router");
-const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
 
 const app = express();
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+app.set("views", "../client/views")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("../client"));
-
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(router);
 app.use(express.json());
 
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
     if(error) throw error;
     else console.log("connected to db");
